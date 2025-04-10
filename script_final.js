b4b382599147f4d0dd15ca359f08c6adaf054b4e
Guiteog <gui.tgomez@gmail.com>
2025-04-09 22:06:08 -0300

    Pontuação rank

--- script_final.js
+++ script_final.js
@@ -1,25 +1,18 @@
-// Simulação dos dados de resultado
-const resultados = [
-    { nome: "X1", pontos: 120, tempo: "45s" },
-    { nome: "X2", pontos: 115, tempo: "48s" },
-    { nome: "X3", pontos: 110, tempo: "50s" },
-    { nome: "X4", pontos: 105, tempo: "55s" },
-    { nome: "X5", pontos: 100, tempo: "60s" },
-  ];
+
   
-  function exibirResultados() {
-    const container = document.getElementById("scoreboard");
-    resultados.forEach((jogador, index) => {
-      const div = document.createElement("div");
-      div.textContent = `${index + 1} ${jogador.nome} (${jogador.pontos} pontos - tempo ${jogador.tempo})`;
-      container.appendChild(div);
-    });
-  }
+function exibirResultados() {
+  let jogador = localStorage.getItem("Jogador");
+  jogador = jogador ? JSON.parse(jogador) : {nome: none, pontuacao:none}; 
+  let rank = document.getElementById('scoreboard');
+  rank.innerHTML = `Nome: ${jogador.nome} Pontos: ${jogador.pontuacao}`;
   
-  function voltarMenu() {
-    // Substitua com o redirecionamento real do seu jogo
-    window.location.href = "index.html";
-  }
   
-  window.onload = exibirResultados;
+}
+
+function voltarMenu() {
+  // Substitua com o redirecionamento real do seu jogo
+  window.location.href = "./login.html";
   
+}
+
+exibirResultados()
