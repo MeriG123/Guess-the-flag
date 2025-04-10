d346499e36d1cbf9845ede70505dfb9bdfa7e87d
Guiteog <gui.tgomez@gmail.com>
2025-04-09 22:21:20 -0300

    Nome de arquivos

--- localStorage.js
+++ localStorage.js
@@ -2,5 +2,5 @@ let nome = document.getElementById('seu_nome');
 
 function dados(){
     localStorage.setItem("Jogador", JSON.stringify({nome:nome.value}));
-    window.location.href = "index.html";
+    window.location.href = "jogo.html";
 }
