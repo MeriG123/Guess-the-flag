252f42e7a5c0948fbf1b67bac7575acae71ff89e
Guiteog <gui.tgomez@gmail.com>
2025-04-15 09:08:10 -0300

    POST

--- localStorage.js
+++ localStorage.js
@@ -1,6 +1,25 @@
 let nome = document.getElementById('seu_nome');
 
-function dados(){
+async function dados(){
     localStorage.setItem("Jogador", JSON.stringify({nome:nome.value}));
-    window.location.href = "jogo.html";
+
+    //Mandar os dados para o servidor
+    try{
+        let resposta = await fetch("http://127.0.0.1:1880/dados",{
+            method: "POST",
+            headers: {"Content-Type": "application/json" },
+            body:JSON.stringify({nome})
+
+        });
+
+        if (!resposta.ok){
+            throw new Error("Erro no envio dos dados");
+        }
+
+        window.location.href = "jogo.html";
+
+    } catch(erro){
+        console.error("Erro ao enviar dados:", erro);
+        alert("Não foi possível enviar os dados")
+    }
 }
